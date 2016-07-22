bfb0a507ad538280f7f26be364d2bf9d62006f9a
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-07-22 17:53:26 -0400

    Migrate to SASS version of material-design-lite

--- src/app/services/api/index.ts
+++ src/app/services/api/index.ts
@@ -2,5 +2,5 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Api {
-  title: string = 'Game of Quotes';
+  title: string = 'redux-observable demo';
 }
--- webpack.config.js
+++ webpack.config.js
@@ -315,7 +315,7 @@ module.exports = (function makeWebpackConfig() {
      * Transforms .scss files to .css
      */
     config.sassLoader = {
-        //includePaths: [path.resolve(__dirname, "node_modules/foundation-sites/scss")]
+        includePaths: [path.resolve(__dirname, "./node_modules/material-design-lite/src")]
     };
 
     /**
